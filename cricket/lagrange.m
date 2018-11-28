fb6efc5bd2fbe8749e3717f601c7194c9c61ee07
Jianwei Han <hanjianwei@gmail.com>
2018-11-28 14:35:24 +0800

    Add lagrange interpolation

--- cricket/lagrange.m
+++ cricket/lagrange.m
@@ -1,3 +1,20 @@
 function p = lagrange(x, y)
 % p = lagrange(x, y)
 % 其中x和y是向量，p是返回的多项式向量
+
+n = length(x);
+
+% pa = (x-x1)(x-x2)...(x-xn)
+pa = poly(x);
+
+p = zeros(1, n);
+
+for i = 1:n
+    % q = (x-xi)
+    q = poly(x(i));
+    % pi = (x-x1)...(x-xi-1)(x-xi+1)...(x-xn)
+    [pi, ~] = deconv(pa, q);
+    % ci = yi / (xi-x1)(xi-x2)...(xi-xi-1)(xi-xi+1)...(xi-xn)
+    ci = y(i) / polyval(pi, x(i));
+    p = p + pi * ci;
+end
